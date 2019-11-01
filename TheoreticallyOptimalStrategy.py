6281c4a6f4d2ecf0ee3a988800d81adf7d199a1b
jielyugt <jie.lyu@gatech.edu>
2019-10-31 21:43:11 -0400

    required python files created

---
+++ TheoreticallyOptimalStrategy.py
@@ -0,0 +1,5 @@
+"""
+Code implementing a TheoreticallyOptimalStrategy object
+It should implement testPolicy() which returns a trades data frame
+The main part of this code should call marketsimcode as necessary to generate the plots used in the report
+"""
---
+++ marketsimcode.py
@@ -0,0 +1,3 @@
+"""
+An improved version of your marketsim code that accepts a "trades" data frame (instead of a file)
+"""
